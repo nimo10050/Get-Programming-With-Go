a1b88e1e28e6b8cc3c9bb2a815da944f8e6666f7
yangyuru <yiming2008@126.com>
2023-01-03 21:33:01 +0800

    minor fix

--- The-Go-Programming-Language/1.5-Fetching-A-URL.go
+++ The-Go-Programming-Language/1.5-Fetching-A-URL.go
@@ -2,19 +2,21 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 )
 
 func main() {
-	for index, arg := range os.Args[0:] {
+	for index, arg := range os.Args[1:] {
 		fmt.Println("index: ", index, "argv: ", arg)
-		//resp, err := http.Get(arg)
-		//if err != nil {
-		//	fmt.Println("network err: ", err)
-		//}
-		//b, err := io.ReadAll(resp.Body)
-		//resp.Body.Close()
-		//fmt.Printf("%s", b)
+		resp, err := http.Get(arg)
+		if err != nil {
+			fmt.Println("network err: ", err)
+		}
+		b, err := io.ReadAll(resp.Body)
+		resp.Body.Close()
+		fmt.Printf("%s", b)
 	}
 
 }
